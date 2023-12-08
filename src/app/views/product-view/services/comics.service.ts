a64219d9cdc74a8e01e946b55821ecc43b1d2e37
hugo delatte <hugo.delatte@protonmail.com>
2023-12-08 10:03:19 +0100

    Refactor product-view component and comics reducer
    The product-view component was updated to import `ComicsState` from `comics.reducer` and replace `comicsFeature.selectLoading` with `comicsFeature.selectComicsLoading`. The constructor now uses `ComicsState` for `store`. Additionally, `selectLoading` has been removed from the `comics.reducer` file.

--- src/app/views/product-view/services/comics.service.ts
+++ src/app/views/product-view/services/comics.service.ts
@@ -1,33 +1,26 @@
-import { Injectable } from '@angular/core';
-import { Observable, map, withLatestFrom } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { Comics } from '../../../model/Comics';
+import {Injectable} from '@angular/core';
+import {Observable, map, withLatestFrom} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {Comics} from '../../../model/Comics';
 
 @Injectable()
 export class ComicsService {
-  constructor(public httpClient: HttpClient) {}
+    constructor(public httpClient: HttpClient) {}
 
-  // Returns Comics without hero names
-  getProduct(): Observable<Comics[]> {
-    return this.httpClient.get<Comics[]>('/assets/data/comics.json');
-  }
+    comics$: Observable<Comics[]> = this.httpClient.get<Comics[]>('/assets/data/comics.json');
 
-  // Return map of title to heroName
-  getComicsNamesToHeroNames(): Observable<{ [title: string]: string }> {
-    return this.httpClient.get<{ [title: string]: string }>(
-      '/assets/data/hero-names.json'
+    comicsNamesToHeroNames: Observable<Record<string, string>> = this.httpClient.get<Record<string, string>>(
+        '/assets/data/hero-names.json'
     );
-  }
 
-  getComicsWithHeroNames$(): Observable<Comics[]> {
-    return this.getProduct().pipe(
-      withLatestFrom(this.getComicsNamesToHeroNames()),
-      map(([comics, heroNames]) => {
-        return comics.map((comic) => ({
-          ...comic,
-          heroName: heroNames[comic.title],
-        }));
-      })
+    comicsWithHeroNames$: Observable<Comics[]> = this.comics$.pipe(
+        withLatestFrom(this.comicsNamesToHeroNames),
+        map(([comics, heroNames]) =>
+            comics.map((comic) => ({
+                ...comic,
+                heroName: heroNames[comic.title],
+            }))
+        )
     );
-  }
+
 }
